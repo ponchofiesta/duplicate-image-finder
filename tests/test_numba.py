15e0a953a61a11a2c1eb07cf5b4318420f4b16dd
Michael Richter <mr@osor.de>
2023-01-03 09:57:01 +0100

    numba bechmark

---
+++ tests/test_numba.py
@@ -0,0 +1,62 @@
+import random
+import numpy as np
+import numba
+
+num_iterations = 6_000_000
+bins = list(range(256))
+
+def input_generator():
+    return random.randint(0, 255)
+    #return np.random.normal(loc=0.0, scale=5.0, size=num_iterations).astype(np.float64)
+
+def test_numpy_histogram(benchmark):
+    a = input_generator()
+    benchmark(np.histogram, a, bins)
+
+def test_numba_histogram(benchmark):
+    a = input_generator()
+    benchmark(numba_histogram, a, bins)
+
+@numba.jit(nopython=True)
+def get_bin_edges(a, bins):
+    bin_edges = np.zeros((bins+1,), dtype=np.float64)
+    a_min = a.min()
+    a_max = a.max()
+    delta = (a_max - a_min) / bins
+    for i in range(bin_edges.shape[0]):
+        bin_edges[i] = a_min + i * delta
+
+    bin_edges[-1] = a_max  # Avoid roundoff error on last point
+    return bin_edges
+
+
+@numba.jit(nopython=True)
+def compute_bin(x, bin_edges):
+    # assuming uniform bins for now
+    n = bin_edges.shape[0] - 1
+    a_min = bin_edges[0]
+    a_max = bin_edges[-1]
+
+    # special case to mirror NumPy behavior for last bin
+    if x == a_max:
+        return n - 1 # a_max always in last bin
+
+    bin = int(n * (x - a_min) / (a_max - a_min))
+
+    if bin < 0 or bin >= n:
+        return None
+    else:
+        return bin
+
+
+@numba.jit(nopython=True)
+def numba_histogram(a, bins):
+    hist = np.zeros((bins,), dtype=np.intp)
+    bin_edges = get_bin_edges(a, bins)
+
+    for x in a.flat:
+        bin = compute_bin(x, bin_edges)
+        if bin is not None:
+            hist[int(bin)] += 1
+
+    return hist, bin_edges
